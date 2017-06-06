b7bdf891138b6e8de34acaebd1dfd5959a5e3a1f
mars137 <atif.tahir13@gmail.com>
2017-06-06 14:18:36 -0700

    Implementation for array partition

--- src/arrayPartition1.py
+++ src/arrayPartition1.py
@@ -6,6 +6,14 @@ class Solution(object):
         """
 
         # your solution here
+        
+        #check if num is not present
+        if not nums:
+        	return
+        
+        nums.sort()
+        sum=0
 
-        return 0
-
+        for i in range(0,len(nums),2):
+        	sum+=nums[i]	 
+        return sum
